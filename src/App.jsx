48ac43820fbb430ffff90a0badb0ee1373692f3a
plabondas001 <plabondas001@gmail.com>
2026-01-13 11:27:01 +0600

    runs

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Runs from './component/Runs'
+import './App.css'
+
+function App() {
+
+
+  return (
+   <>
+   <Runs></Runs>
+   </>
+  )
+}
+
+export default App
---
+++ src/component/Runs.jsx
@@ -0,0 +1,10 @@
+const Runs = () => {
+ return (
+    <div>
+        <h1>Runs :</h1>
+        <button>Single Runs</button>
+    </div>
+ )
+};
+
+export default Runs;
